d67247cda6a3dec02b8a4a003757710b0b9d304b
Nilofar <nilofar.moradi.farisar@gmail.com>
2023-10-30 11:05:42 +0330

    First_think

---
+++ student_teacher.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Define the teacher model that outputs the loss function
+class TeacherModel(nn.Module):
+    def __init__(self):
+        super(TeacherModel, self).__init__()
+        self.fc1 = nn.Linear(10, 10)
+        self.fc2 = nn.Linear(10, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define the student model that uses the dynamic loss function
+class StudentModel(nn.Module):
+    def __init__(self):
+        super(StudentModel, self).__init__()
+        self.fc1 = nn.Linear(10, 10)
+        self.fc2 = nn.Linear(10, 1)
+
+    def forward(self, x, loss_fn):
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        loss = loss_fn(x)
+        return x, loss
+
+# Define the dynamic loss function
+def dynamic_loss_fn(output):
+    if output.mean() > 0:
+        return nn.MSELoss()(output, torch.ones_like(output))
+    else:
+        return nn.MSELoss()(output, torch.zeros_like(output))
+
+# Initialize the models and optimizer
+teacher_model = TeacherModel()
+student_model = StudentModel()
+optimizer = optim.SGD(student_model.parameters(), lr=0.1)
+
+# Train the student model using the dynamic loss function
+for i in range(100):
+    optimizer.zero_grad()
+    x = torch.randn(1, 10)
+    y = torch.randn(1, 1)
+    teacher_loss_fn = nn.MSELoss()
+    teacher_output = teacher_model(x)
+    student_output, student_loss_fn = student_model(x, dynamic_loss_fn(teacher_output))
+    teacher_loss = teacher_loss_fn(teacher_output, y)
+    student_loss = student_loss_fn(student_output, y)
+    teacher_loss.backward()
+    student_loss.backward()
+    optimizer.step()
